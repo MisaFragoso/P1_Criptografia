028f380e4ed5a05b62cd7422c651e3bb53ebbf52
RGJG28 <54247546+RGJG28@users.noreply.github.com>
2024-04-13 20:24:00 -0600

    actualizacion
    No funciona el subir la llave privada, se tiene que revisar las condiciones en el def login.
    No funciona el firmado, dice que es invalido, no sé por qué.
    El usuario 2 entra directamente al chat.

--- main5.py
+++ main5.py
@@ -17,6 +17,9 @@ messages = []
 symmetric_key = None
 private_key = None
 public_key = None
+sesion = 0
+secret = None
+
 
 def sign_message(message, private_key):
     key = private_key
@@ -31,8 +34,8 @@ def encrypt_symmetric(message, key):
     return ciphertext, tag, nonce
 
 def encrypt_asymmetric(data, public_key):
-    recipient_key = public_key
-    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    #Cifrado asimétrico basado en RSA y el relleno OAEP
+    cipher_rsa = PKCS1_OAEP.new(public_key)
     encrypted_data = cipher_rsa.encrypt(data)
     return encrypted_data
 
@@ -50,6 +53,49 @@ def pbkdf(password, salt, length):
         backend=default_backend()
     )
     return kdf.derive(password)
+
+def create_keys(secret, private_key_path,public_key_path):
+    key = RSA.generate(2048)
+    # Exportamos la clave privada
+    private_key = key.export_key(passphrase=secret)
+    # Obtenemos la clave pública
+    public_key = key.publickey().export_key()
+    # Guardar la clave privada en la ubicación especificada por el usuario
+    with open(private_key_path, "wb") as f:
+        f.write(private_key)
+    # Guardar la clave publica en directorio de usuarios (falta implementar)
+    with open(public_key_path, "wb") as f:
+        f.write(public_key)
+    send_file(private_key_path, as_attachment=True)
+    send_file(public_key_path, as_attachment=True)
+    return render_template("login.html")
+
+def importing_keys(private_key_path, public_key_path, secret):
+    try:
+        # Importar clave privada con contraseña
+        with open(private_key_path, "rb") as f:
+            private_key_data = f.read()  # Leer los datos completos de la clave como bytes
+        private_key = RSA.import_key(private_key_data, passphrase=secret)
+        print("Clave privada importada correctamente")
+
+        # Leer datos de clave pública de un archivo
+        with open(public_key_path, "rb") as f:
+            public_key_data = f.read()
+
+        # Asegurarse de que los datos estén en el formato correcto
+        try:
+            public_key = RSA.import_key(public_key_data)
+            # Si no hay excepción, los datos están en el formato correcto
+            print("Clave pública importada correctamente")
+        except (ValueError, IndexError) as e:
+            # Manejar el error si los datos no están en el formato correcto
+            print(f"Error al importar la clave pública: {e}")
+        return private_key, public_key
+
+    except (IOError, ValueError) as e:
+        # Manejar errores de importación de claves
+        print(f"Error al importar las claves: {e}")
+        return None, None
 
 @app.route("/")
 def index():
@@ -70,93 +116,56 @@ def login():
     global symmetric_key, private_key, public_key
     secret = request.form["secret"]
     key_location = request.form["key_location"]
-
-    if not secret and not request.files['private_key']: 
-        return redirect("/")
-
     private_key_path = os.path.join(key_location, "private.pem")
     public_key_path = os.path.join(key_location, "public.pem")
+    #sesion = 1
+    if not secret and not request.files['private_key']: 
+        return redirect("/")
+    if secret and not request.files['private_key'] :
+        create_keys(secret, private_key_path, public_key_path)
+        
+        private_key, public_key = importing_keys(private_key_path, public_key_path, secret)
+        
+        if private_key is None:
+            print("Error al importar las llaves")
+            return redirect ("/")
 
-    if os.path.exists(public_key_path and private_key_path):
-        try:
-            # Importar clave privada con contraseña
-            with open(private_key_path, "rb") as f:
-                private_key_data = f.read()  # Leer los datos completos de la clave como bytes
-            private_key = RSA.import_key(private_key_data, passphrase=secret)
-
-            # Leer datos de clave pública de un archivo
-            with open(public_key_path, "rb") as f:
-                public_key_data = f.read()
-
-            # Asegurarse de que los datos estén en el formato correcto
-            try:
-                public_key = RSA.import_key(public_key_data)
-                # Si no hay excepción, los datos están en el formato correcto
-                print("Clave pública importada correctamente")
-            except (ValueError, IndexError) as e:
-                # Manejar el error si los datos no están en el formato correcto
-                print(f"Error al importar la clave pública: {e}")
-
-        except (IOError, ValueError) as e:
-            # Manejar errores de importación de claves
-            print(f"Error al importar las claves: {e}")
-            return redirect("/")
-    else:
-        # Generar pareja de claves RSA de 2048 bits de longitud, llaves asimetricas
-        key = RSA.generate(2048)
-        # Exportamos la clave privada
-        private_key = key.export_key(passphrase=secret)
-        # Obtenemos la clave pública
-        public_key = key.publickey().export_key()
-        # Guardar la clave privada en la ubicación especificada por el usuario
-        private_key_path = os.path.join(key_location, "private.pem")
-        with open(private_key_path, "wb") as f:
-             f.write(private_key)
-        # Guardar la clave publica en dircetorio de usuarios (falta implementar)
-        public_key_path = os.path.join(key_location, "public.pem")
-        with open(public_key_path, "wb") as f:
-             f.write(public_key)
-        send_file(private_key_path, as_attachment=True)
-        send_file(public_key_path, as_attachment=True)
-
-        # Derivar clave simétrica usando PBKDF2
-        password = bytes(secret, 'utf-8')
-        salt = get_random_bytes(16)
-        symmetric_key = pbkdf(password, salt, 32)
-
-        # Imprimir información en la terminal
-        print("Clave privada RSA:")
-        #print(private_key.export_key().decode())
-        print("\nClave pública RSA:")
-        #print(public_key.export_key().decode())
-        print("\nSalt generado:", salt.hex())
-        print("Clave simétrica derivada:", symmetric_key.hex())
-
+    # Derivar clave simétrica usando PBKDF2
+    password=bytes(secret, 'utf-8')
+    salt = get_random_bytes(16)
+    symmetric_key = pbkdf(password, salt, 32)
+    # Imprimir información en la terminal
+    print("Clave privada RSA:")
+    print(private_key.export_key().decode())
+    print("\nClave pública RSA:")
+    print(public_key.export_key().decode())
+    print("\nSalt generado:", salt.hex())
+    print("Clave simétrica derivada:", symmetric_key.hex())
+    
     return redirect("/chat")
 
 @app.route("/send_message", methods=["POST"])
 def send_message():
     global messages, symmetric_key, private_key, public_key
+
     if not symmetric_key:
         return jsonify({"error": "Debes iniciar sesión para enviar mensajes."}), 403
 
     username = request.form["username"]
     message = request.form["message"]
 
-    # Cifrar mensaje simétricamente
-    ciphertext, tag, nonce = encrypt_symmetric(message, symmetric_key)
-
-    # Importar clave pública desde bytes (asumiendo que ya has leído los datos de la clave pública)
-    #public_key_object = RSA.import_key(public_key)  # Usar los datos brutos de bytes aquí
-
-    # Cifrar clave simétrica con clave pública
-    encrypted_symmetric_key = encrypt_asymmetric(symmetric_key, public_key)
-
-    # Hash del mensaje
+    # Hash the message before signing
     message_hash = hash_message(message)
 
-    # Firma digital del mensaje (considera usar relleno PSS)
+    # Sign the message hash using the private key
     signature = sign_message(message_hash, private_key)
+
+    # Encrypt the message using the symmetric key
+    ciphertext, tag, nonce = encrypt_symmetric(message, symmetric_key)
+
+
+    # Encrypt the symmetric key with the public key
+    encrypted_symmetric_key = encrypt_asymmetric(symmetric_key, public_key)
 
     messages.append({
         "username": username,
@@ -169,16 +178,16 @@ def send_message():
         "signature": signature
     })
 
-    # Imprimir información en la terminal
+    # Print information to the console
     print("\nMensaje cifrado RSA:", base64.b64encode(encrypted_symmetric_key).decode())
     print("Mensaje descifrado:", message)
     print("Tag:", base64.b64encode(tag).decode())
     print("Nonce (IV):", base64.b64encode(nonce).decode())
     print("Mensaje integro")
 
-    # Verificar si la firma digital es válida
+    # Verify the digital signature using the public key and the hashed message
     try:
-        pkcs1_15.new(public_key).verify(SHA256.new(message.encode()), signature)
+        pkcs1_15.new(public_key).verify(SHA256.new(message_hash), signature)
         print("La firma es válida.")
     except (ValueError, TypeError):
         print("La firma no es válida.")
@@ -204,6 +213,10 @@ def get_messages():
         serialized_messages.append(serialized_message)
     return jsonify({"messages": serialized_messages})
 
+@app.route("/user2")
+def user2():
+  return render_template("login.html")
+
 @app.route("/logout", methods=['POST'])
 def logout():
     global symmetric_key, private_key, public_key, messages
@@ -211,6 +224,7 @@ def logout():
     private_key = None
     public_key = None
     messages.clear()
+    sesion = 0
     return redirect("/")
 
 if __name__ == "__main__":
