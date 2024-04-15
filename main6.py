3bca4d182112e06ea7d7953127265e062f600120
DrownedAbbb <78518485+DrownedAbbb@users.noreply.github.com>
2024-04-14 22:30:57 -0600

    Se carga la ultima versión del código

--- main6.py
+++ main6.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 # SECURE CHAT - DEMO
 #
-# Authors: Jimenez García Rodrigo Gaudencio
+# Authors: - Fragoso Alarcón Alejandro Misael   315077757
+#          - Jimenez García Rodrigo Gaudencio   317296103
+#          - Oronzor Montes Manases Leonel      421099535
+#          - Pérez Pérez Alberto Guadalupe      317022832
+#          - Ramos Rosas Luis Carlos	        317226944
 # Date: 2024.04.04
 # License: MIT
 
-from flask import Flask, render_template, request, jsonify, redirect, send_file
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, jsonify, redirect
 from Cryptodome.PublicKey import RSA
 from Cryptodome.Random import get_random_bytes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -19,48 +22,61 @@ from Cryptodome.Signature import pkcs1_15
 from Cryptodome.Hash import SHA256
 import base64
 
-app = Flask(__name__)
+app = Flask(__name__)  # Inicializa la aplicación Flask
 
-users = {}
+# Variables para almacenar usuarios, mensajes, y claves criptográficas
+users = {} 
 messages = []
 symmetric_key = None
 private_key = None
 public_key = None
-session_id = 0  # Corrected variable name
-secret = None
 
-# Función para firmar un mensaje
-def sign_message(message, private_key):
-    hasher = SHA256.new(message.encode())
+# Función para firmar un mensaje hash con una clave privada
+def sign_message(message_hash, private_key):
+    hasher = SHA256.new(bytes.fromhex(message_hash))  
     signature = pkcs1_15.new(private_key).sign(hasher)
     return signature
 
-
+# Función para encriptar un mensaje usando una clave simétrica
 def encrypt_symmetric(message, key):
     cipher = AES.new(key, AES.MODE_GCM)
     ciphertext, tag = cipher.encrypt_and_digest(message.encode())
     nonce = cipher.nonce
     return ciphertext, tag, nonce
 
+# Función para desencriptar un mensaje usando una clave simétrica
+def decrypt_symmetric(ciphertext, tag, nonce, key):
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    try:
+        plaintext = cipher.decrypt_and_verify(ciphertext, tag)
+        return plaintext.decode('utf-8')
+    except ValueError as e:
+        print("Decryption failed or tag verification failed:", e)
+        return None
 
+# Función para encriptar datos con clave pública RSA
 def encrypt_asymmetric(data, public_key):
-    cipher_rsa = PKCS1_OAEP.new(public_key)
-    encrypted_data = cipher_rsa.encrypt(data)
-    return encrypted_data
+    try:
+        cipher_rsa = PKCS1_OAEP.new(public_key)
+        encrypted_data = cipher_rsa.encrypt(data)
+        return encrypted_data
+    except Exception as e:
+        print("Failed to encrypt with RSA:", str(e))
+        return None
 
-
+# Función para desencriptar datos con clave privada RSA
 def decrypt_asymmetric(encrypted_data, private_key):
     cipher_rsa = PKCS1_OAEP.new(private_key)
     data = cipher_rsa.decrypt(encrypted_data)
     return data
 
-
+# Función para hashear un mensaje
 def hash_message(message):
     hash_object = hashlib.sha256()
-    hash_object.update(message.encode())
+    hash_object.update(message) 
     return hash_object.hexdigest()
 
-
+# Función para derivar una clave utilizando PBKDF2
 def pbkdf2(password, salt, length):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -71,7 +87,7 @@ def pbkdf2(password, salt, length):
     )
     return kdf.derive(password)
 
-
+# Función para crear claves RSA y almacenarlas en archivos
 def create_keys(secret, private_key_path, public_key_path):
     try:
         key = RSA.generate(2048)
@@ -86,7 +102,7 @@ def create_keys(secret, private_key_path, public_key_path):
         print(f"Error creating keys: {e}")
         return None
 
-
+# Función para importar claves desde archivos
 def import_keys(private_key_path, public_key_path, secret):
     try:
         with open(private_key_path, "rb") as f:
@@ -102,25 +118,27 @@ def import_keys(private_key_path, public_key_path, secret):
         print(f"Error importing keys: {e}")
         return None, None
 
-
-
+# Rutas y controladores de Flask para la aplicación web
 @app.route("/")
 def index():
     if symmetric_key:
-        return redirect("/user2")  # Redirige a /user2 si ya hay una sesión activa
+        return redirect("/user2")
     else:
         return render_template("login.html")
 
-
 @app.route("/chat")
 def chat():
+    # Verifica si existe una clave simétrica; si existe, carga la página del chat.
+    # Si no existe, redirige al usuario a la página de inicio para que inicie sesión.
     if symmetric_key:
         return render_template("index.html", messages=messages, users=users)
     else:
         return redirect("/")
-    
+
 @app.route("/login", methods=["POST"])
 def login():
+    # Procesa la información de login, crea o importa claves RSA y establece una clave simétrica.
+    # Guarda las claves y el nombre de usuario en el diccionario 'users'.
     global symmetric_key, private_key, public_key
     secret = request.form["secret"]
     username = request.form["username"]
@@ -128,45 +146,15 @@ def login():
     private_key_path = os.path.join(key_location, f"{username}_private.pem")
     public_key_path = os.path.join(key_location, f"{username}_public.pem")
 
-    # Verifica si se proporcionó un secreto y se subió un archivo de clave privada
     if secret and 'private_key' in request.files:
-        # Crea las claves RSA si aún no existen
         create_keys(secret, private_key_path, public_key_path)
 
-    # Intenta importar las claves privada y pública
     private_key, public_key = import_keys(private_key_path, public_key_path, secret)
 
-    # Verifica si las claves se importaron correctamente
     if private_key is None or public_key is None:
         print("Error importing keys")
         return redirect("/")
 
-    # Genera la clave simétrica
-    password = bytes(secret, 'utf-8')
-    salt = get_random_bytes(16)
-    symmetric_key = pbkdf2(password, salt, 32)
-
-    # Almacena las claves y la clave simétrica en la sesión del usuario
-    users[username] = {
-        "symmetric_key": symmetric_key,
-        "private_key": private_key,
-        "public_key": public_key
-    }
-
-    print("RSA private key:")
-    print(private_key.export_key().decode())
-    print("\nRSA public key:")
-    print(public_key.export_key().decode())
-    print("\nGenerated salt:", salt.hex())
-    print("Derived symmetric key:", symmetric_key.hex())
-
-    return redirect("/chat")
-
-
-    # Si no se proporciona un secreto ni se carga un archivo de clave privada, redirige a la página principal
-    if not secret and not request.files['private_key']:
-        return redirect("/")
-
     password = bytes(secret, 'utf-8')
     salt = get_random_bytes(16)
     symmetric_key = pbkdf2(password, salt, 32)
@@ -177,18 +165,11 @@ def login():
         "public_key": public_key
     }
 
-    print("RSA private key:")
-    print(private_key.export_key().decode())
-    print("\nRSA public key:")
-    print(public_key.export_key().decode())
-    print("\nGenerated salt:", salt.hex())
-    print("Derived symmetric key:", symmetric_key.hex())
-
     return redirect("/chat")
-
 
 @app.route("/send_message", methods=["POST"])
 def send_message():
+    # Maneja el envío de mensajes cifrados entre usuarios. Los mensajes se cifran simétricamente y se añaden a la lista de mensajes.
     global messages, symmetric_key, users
 
     if not symmetric_key:
@@ -198,74 +179,85 @@ def send_message():
     recipient = request.form["recipient"]
     message = request.form["message"]
 
-    # Verifica que tanto el remitente como el destinatario están registrados y tienen claves
     if sender not in users or recipient not in users:
-        print(f"One of the users is not registered: sender={sender}, recipient={recipient}")
         return jsonify({"error": "Sender or recipient not registered."}), 403
-
     try:
         ciphertext, tag, nonce = encrypt_symmetric(message, users[recipient]["symmetric_key"])
         message_hash = hash_message(ciphertext)
         signature = sign_message(message_hash, users[sender]["private_key"])
         encrypted_symmetric_key = encrypt_asymmetric(users[recipient]["symmetric_key"], users[recipient]["public_key"])
+
+        if encrypted_symmetric_key is None:
+            return jsonify({"error": "Failed to encrypt symmetric key."}), 500
+
+        messages.append({
+            "sender": sender,
+            "recipient": recipient,
+            "message": message,
+            "ciphertext": ciphertext,
+            "tag": tag,
+            "nonce": nonce,
+            "encrypted_symmetric_key": encrypted_symmetric_key,
+            "message_hash": message_hash,
+            "signature": signature
+        })
+        print("Encrypted RSA message:", base64.b64encode(encrypted_symmetric_key).decode())
+        print("Decrypted message:", message)
+        print("Tag:", base64.b64encode(tag).decode())
+        print("Nonce (IV):", base64.b64encode(nonce).decode())
+        print("Hashed Message:", message_hash)
     except Exception as e:
-        print(f"Error encrypting message: {e}")
-        return jsonify({"error": "Encryption failed."}), 500
-
-    messages.append({
-        "sender": sender,
-        "recipient": recipient,
-        "message": message,
-        "ciphertext": ciphertext,
-        "tag": tag,
-        "nonce": nonce,
-        "encrypted_symmetric_key": encrypted_symmetric_key,
-        "message_hash": message_hash,
-        "signature": signature
-    })
-
-    print("\nEncrypted RSA message:", base64.b64encode(encrypted_symmetric_key).decode())
-    print("Decrypted message:", message)
-    print("Tag:", base64.b64encode(tag).decode())
-    print("Nonce (IV):", base64.b64encode(nonce).decode())
-    print("Hashed Message: ",message_hash)
-    print("Message integrity:")
-
+        print("Error during message sending:", str(e))
+        return jsonify({"error": "An error occurred during message sending."}), 500
+    
     try:
-        pkcs1_15.new(users[sender]["public_key"]).verify(SHA256.new(message_hash.encode()), signature)
+        verifier = pkcs1_15.new(users[sender]["public_key"])
+        hasher = SHA256.new(bytes.fromhex(message_hash))
+        verifier.verify(hasher, signature)
         print("Signature is valid.")
     except (ValueError, TypeError):
         print("Signature is invalid.")
         return jsonify({"error": "Signature verification failed."}), 500
-
     return jsonify({"sender": sender, "recipient": recipient, "message": message})
 
 @app.route("/get_messages")
 def get_messages():
+    # Retorna todos los mensajes en un formato serializado, incluyendo información sobre la criptografía utilizada.
     global messages
     serialized_messages = []
     for message in messages:
+        base64_ciphertext = base64.b64encode(message["ciphertext"]).decode('utf-8')
+        base64_tag = base64.b64encode(message["tag"]).decode('utf-8')
+        base64_nonce = base64.b64encode(message["nonce"]).decode('utf-8')
+        base64_encrypted_symmetric_key = base64.b64encode(message["encrypted_symmetric_key"]).decode('utf-8')
+        base64_signature = base64.b64encode(message["signature"]).decode('utf-8')
+
+        decrypted_message = decrypt_symmetric(message["ciphertext"], message["tag"], message["nonce"], users[message["recipient"]]["symmetric_key"])
+        message_text = decrypted_message if decrypted_message else "Failed to decode message"
+
         serialized_message = {
             "sender": message["sender"],
             "recipient": message["recipient"],
-            "message": message["message"],
-            "ciphertext": base64.b64encode(message["ciphertext"]).decode(),
-            "tag": base64.b64encode(message["tag"]).decode(),
-            "nonce": base64.b64encode(message["nonce"]).decode(),
-            "encrypted_symmetric_key": base64.b64encode(message["encrypted_symmetric_key"]).decode(),
+            "message": message_text,
+            "ciphertext": base64_ciphertext,
+            "tag": base64_tag,
+            "nonce": base64_nonce,
+            "encrypted_symmetric_key": base64_encrypted_symmetric_key,
             "message_hash": message["message_hash"],
-            "signature": base64.b64encode(message["signature"]).decode()
+            "signature": base64_signature
         }
         serialized_messages.append(serialized_message)
-    return jsonify({"messages": serialized_messages})
 
+    return jsonify({"messages": serialized_messages})
 
 @app.route("/user2")
 def user2():
-  return render_template("login.html")
+    # Redirecciona al usuario a la página de inicio si aún no se ha autenticado con una clave simétrica.
+    return render_template("login.html")
 
 @app.route("/logout", methods=['POST'])
 def logout():
+    # Limpia todas las claves y mensajes al cerrar sesión.
     global symmetric_key, private_key, public_key, messages, users
     symmetric_key = None
     private_key = None
@@ -273,7 +265,6 @@ def logout():
     messages.clear()
     users.clear()
     return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
